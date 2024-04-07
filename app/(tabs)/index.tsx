ee642ec392d29f1144790f84d65ff792578a816e
Sam Svindland <sam@Sams-MacBook-Pro.local>
2024-04-07 17:05:31 -0400

    get images and loading working correctly

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,96 +1,103 @@
-import {
-  Text,
-  Image,
-  Keyboard,
-  ScrollView,
-  Pressable,
-  TouchableWithoutFeedback,
-} from "react-native";
+import { Image, Keyboard, FlatList } from "react-native";
 
 import { View } from "@/components/Themed";
 import { useState } from "react";
 import { Slider } from "@miblanchard/react-native-slider";
-import { Link } from "expo-router";
 import { Button, SegmentedButtons, TextInput } from "react-native-paper";
+import { useQuery } from "@tanstack/react-query";
+import * as FileSystem from "expo-file-system";
 
 export default function TabOneScreen() {
   const [model, setModel] = useState<string>("default");
+  const [searchModel, setSearchModel] = useState<string>("default");
   const [text, onChangeText] = useState("");
   const [searchText, setSearchText] = useState("");
   const [number, setNumber] = useState<number>(1);
+  const [searchNumber, setSearchNumber] = useState<number>(1);
+
+  const query = useQuery({
+    queryKey: ["Images", searchModel, searchText, searchNumber],
+    queryFn: async () => {
+      const images = [];
+
+      for (let i = 0; i < searchNumber; ++i) {
+        const file = await FileSystem.downloadAsync(
+          `https://swsvindland--${searchModel}-fastapi-app.modal.run/?prompt=${searchText}`,
+          FileSystem.documentDirectory + searchModel + searchText + i + ".jpeg",
+          {},
+        );
+
+        images.push(file.uri);
+      }
+
+      return images;
+    },
+  });
 
   const handleSubmit = () => {
     setSearchText(text);
+    setSearchNumber(number);
+    setSearchModel(model);
     Keyboard.dismiss();
   };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View className="flex-1 items-center justify-center p-4">
-        <SegmentedButtons
-          value={model}
-          onValueChange={setModel}
-          buttons={[
-            {
-              value: "default",
-              label: "Default",
-            },
-            {
-              value: "realistic",
-              label: "Realistic",
-            },
-            { value: "anime", label: "Anime" },
-          ]}
+    <View className="flex-1 items-center justify-center px-4 pt-4">
+      <SegmentedButtons
+        value={model}
+        onValueChange={setModel}
+        buttons={[
+          {
+            value: "default",
+            label: "Default",
+          },
+          {
+            value: "realistic",
+            label: "Realistic",
+          },
+          { value: "anime", label: "Anime" },
+        ]}
+      />
+      <View className="flex flex-row justify-center">
+        <TextInput
+          label="Prompt"
+          className="flex-1 my-2"
+          onChangeText={onChangeText}
+          value={text}
+          multiline
+          numberOfLines={4}
         />
-        <View className="flex flex-row justify-center">
-          <TextInput
-            label="Prompt"
-            className="flex-1 my-2"
-            onChangeText={onChangeText}
-            value={text}
-            multiline
-            numberOfLines={4}
-          />
-        </View>
-        <View className="w-full mx-2 align-stretch justify-center px-4 py-2">
-          <Slider
-            value={number}
-            onValueChange={(value) => setNumber(value[0])}
-            step={1}
-            minimumValue={1}
-            maximumValue={4}
-          />
-          <Text>{number}</Text>
-        </View>
-        <View className="w-full justify-center pt-8 pb-2 px-4">
-          <Button mode="contained" onPress={handleSubmit}>
-            Submit
-          </Button>
-        </View>
-        <ScrollView>
-          <View className="flex flex-col justify-center gap-4">
-            {Array.from(Array(number).keys()).map((index) => (
-              <Link key={index} href="/modal" asChild>
-                <Pressable>
-                  {({ pressed }) => (
-                    <Image
-                      className={
-                        pressed
-                          ? "w-96 h-96 rounded-2xl shadow opacity-80"
-                          : "w-96 h-96 rounded-2xl shadow"
-                      }
-                      source={{
-                        uri: `https://swsvindland--${model}-fastapi-app.modal.run/?prompt=${searchText}`,
-                        // uri: "http://m.gettywallpapers.com/wp-content/uploads/2023/05/Cool-Anime-Pfp.jpg",
-                      }}
-                    />
-                  )}
-                </Pressable>
-              </Link>
-            ))}
-          </View>
-        </ScrollView>
       </View>
-    </TouchableWithoutFeedback>
+      <View className="w-full mx-2 justify-center px-4 py-2">
+        <Slider
+          value={number}
+          onValueChange={(value) => setNumber(value[0])}
+          step={1}
+          minimumValue={1}
+          maximumValue={4}
+        />
+      </View>
+      <View className="w-full justify-center pb-2 px-4">
+        <Button
+          mode="contained"
+          onPress={handleSubmit}
+          loading={query.isLoading}
+        >
+          Generate {number} Images
+        </Button>
+      </View>
+      <FlatList
+        data={query.data}
+        renderItem={({ item }) => (
+          <Image
+            className="w-96 h-96 rounded-2xl shadow my-2"
+            source={{
+              uri: item,
+            }}
+          />
+        )}
+        keyExtractor={(item) => item}
+      />
+    </View>
   );
 }
