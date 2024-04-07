27cf4805c1ae2d84ca9393d4a9f6aee7c56f72be
Sam Svindland <sam@Sams-MacBook-Pro.local>
2024-04-07 17:13:34 -0400

    update icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,14 +30,14 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Generate",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="image" color={color} />,
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
           title: "Settings",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} />,
         }}
       />
     </Tabs>
