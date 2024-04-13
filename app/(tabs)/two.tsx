7299185808c78c89d447cd3bbbf1064bfde943ec
Sam Svindland <sam@Sams-MacBook-Pro.local>
2024-04-13 16:48:31 -0400

    add links

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -4,6 +4,7 @@ import RevenueCatUI from "react-native-purchases-ui";
 import Purchases from "react-native-purchases";
 import { useQuery } from "@tanstack/react-query";
 import { Text } from "react-native";
+import { ExternalLink } from "@/components/ExternalLink";
 
 async function sleep(ms: number): Promise<void> {
   await new Promise((resolve) => setTimeout(resolve, ms));
@@ -30,14 +31,42 @@ export default function TabTwoScreen() {
   return (
     <View className="flex-1">
       {premiumQuery.data ? (
-        <View className="flex w-full h-full justify-center items-center p-4">
-          <Text className="dark:text-white">
-            You are subscribed to Premium. Manage your subscription in the apple
-            app store.
-          </Text>
-        </View>
+        <>
+          <View className="flex items-center justify-center px-4 pt-4">
+            <ExternalLink href="https://svindland.dev/privacy">
+              <Text className="uppercase underline bold text-purple-500">
+                Privacy Policy
+              </Text>
+            </ExternalLink>
+            <ExternalLink href="https://svindland.dev/terms">
+              <Text className="uppercase underline bold text-purple-500">
+                Terms
+              </Text>
+            </ExternalLink>
+          </View>
+          <View className="flex w-full h-full justify-center items-center p-4">
+            <Text className="dark:text-white">
+              You are subscribed to Premium. Manage your subscription in the
+              apple app store.
+            </Text>
+          </View>
+        </>
       ) : (
-        <RevenueCatUI.Paywall onDismiss={() => premiumQuery.refetch()} />
+        <View className="flex-1">
+          <View className="flex items-center justify-center px-4 pt-4">
+            <ExternalLink href="https://svindland.dev/privacy">
+              <Text className="uppercase underline bold text-purple-500">
+                Privacy Policy
+              </Text>
+            </ExternalLink>
+            <ExternalLink href="https://svindland.dev/terms">
+              <Text className="uppercase underline bold text-purple-500">
+                Terms
+              </Text>
+            </ExternalLink>
+          </View>
+          <RevenueCatUI.Paywall onDismiss={() => premiumQuery.refetch()} />
+        </View>
       )}
     </View>
   );
